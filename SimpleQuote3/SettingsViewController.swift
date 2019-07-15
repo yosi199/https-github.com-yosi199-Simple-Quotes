01d94d2544ced6a08770ccab1f04348091b82efc
Yosi Mizrachi <yosimizrachi7@gmail.com>
2019-07-15 09:42:21 +0300

    more work on settings

--- SimpleQuote3/SettingsViewController.swift
+++ SimpleQuote3/SettingsViewController.swift
@@ -7,15 +7,62 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIDropInteractionDelegate, FileHandler {
     
 
     @IBOutlet weak var logo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let dropInteraction = UIDropInteraction(delegate: self)
+        logo.isUserInteractionEnabled = true
+        logo.addInteraction(dropInteraction)
+        setLogo()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.hasItemsConforming(toTypeIdentifiers: [kUTTypeImage as String]) && session.items.count == 1
+//        return session.canLoadObjects(ofClass: UIImage.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        let dropLocation = session.location(in: view)
+        
+        let operation: UIDropOperation
+        
+        if logo.frame.contains(dropLocation) {
+            /*
+             If you add in-app drag-and-drop support for the .move operation,
+             you must write code to coordinate between the drag interaction
+             delegate and the drop interaction delegate.
+             */
+            operation = .copy
+        } else {
+            // Do not allow dropping outside of the image view.
+            operation = .cancel
+        }
+        
+        return UIDropProposal(operation: operation)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        // Consume drag items (in this example, of type UIImage).
+        session.loadObjects(ofClass: UIImage.self) { imageItems in
+            let images = imageItems as! [UIImage]
+            /*
+             If you do not employ the loadObjects(ofClass:completion:) convenience
+             method of the UIDropSession class, which automatically employs
+             the main thread, explicitly dispatch UI work to the main thread.
+             For example, you can use `DispatchQueue.main.async` method.
+             */
+            self.logo.image = images.first
+            self.saveImageFile(data: images.first?.pngData(), withName: COMPANY_LOGO)
+        }
     }
     
     override var preferredContentSize: CGSize {
@@ -43,5 +90,11 @@ class SettingsViewController: UIViewController {
     
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    private func setLogo(){
+        if let image = UIImage(contentsOfFile: getFileForName(name: COMPANY_LOGO).path){
+            logo.image = image
+        }
     }
 }
