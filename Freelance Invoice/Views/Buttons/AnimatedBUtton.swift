58b2a2c3e9ae70412a17928633a6133723120ad3
Yosi Mizrachi <yosimizrachi7@gmail.com>
2019-10-15 13:30:40 +0300

    Pushing all header files from pods

---
+++ Freelance Invoice/Views/Buttons/AnimatedBUtton.swift
@@ -0,0 +1,29 @@
+//
+//  AnimatedBUtton.swift
+//  Freelance Invoice
+//
+//  Created by Yosi Mizrachi on 15/10/2019.
+//  Copyright © 2019 Yosi Mizrachi. All rights reserved.
+//
+
+import UIKit
+
+class AnimatedBUtton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("coder Not implemented")
+    }
+
+}
