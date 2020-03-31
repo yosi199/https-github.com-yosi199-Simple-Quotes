41b81a1e3a7bc65f957411ca77b2a96fb1926296
Yosi <yosimizrachi7@gmail.com>
2020-03-31 09:42:56 +0300

    just pushing all files

---
+++ Pods/TPPDF/Source/API/Utils/PDFCopy.swift
@@ -0,0 +1,23 @@
+//
+//  PDFCopy.swift
+//  TPPDF
+//
+//  Created by Philip Niedertscheider on 06.04.18.
+//
+
+/**
+ TODO: Documentation
+ */
+public protocol PDFCopy {
+
+    /**
+     TODO: Documentation
+     */
+    associatedtype Element
+
+    /**
+     TODO: Documentation
+     */
+    var copy: Element { get }
+
+}
